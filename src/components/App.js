5f90b958fa1319be965bd6cbea8fe8a434795a6b
Ian Andrewson <ianandrewson@gmail.com>
2020-01-28 14:08:06 -0800

    Footer refactored, snapshot passes

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import styles from './App.css';
 import Header from './Header.js';
-
-const Footer = () => (
-  <footer className={styles.Footer}>
-    <section className={styles.stats}>
-      <p>Characters: 493</p>
-      <p>Locations: 76</p>
-      <p>Episodes: 31</p>
-    </section>
-    <section className={styles.acknowledgment}>
-      <p>❮❯ by <a href="#">Axel Fuhrmann</a> 2020</p>
-    </section>
-  </footer>
-);
+import Footer from './Footer.js';
 
 const App = () => (
   <React.Fragment>
